7fd9f7f9343397eaab0fcddad8cad1f6f93ddd41
Leo Lu <llu@lcs.on.ca>
2022-05-10 19:26:20 -0400

    Initial Commit

---
+++ ReceipeSearcher/ReceipeSearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceipeSearcherApp.swift
+//  ReceipeSearcher
+//
+//  Created by Leo Lu on 2022-05-10.
+//
+
+import SwiftUI
+
+@main
+struct ReceipeSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
