9b62373e7fe398b0754394e80b320b5f66eedcc6
Leo Lu <llu@lcs.on.ca>
2022-05-11 11:54:46 -0400

    added static layout for the search view

---
+++ ReceipeSearcher/SearchView.swift
@@ -0,0 +1,35 @@
+//
+//  SearchView.swift
+//  ReceipeSearcher
+//
+//  Created by Leo Lu on 2022-05-10.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        NavigationView{
+            VStack{
+                Text("Search Recipe")
+                    .font(.title)
+                
+                Form {
+                    Section{
+                        Text("enter")
+                    }
+                }
+                
+                Spacer()
+                
+            }
+        }
+        .navigationTitle("Search recipe")
+    }
+}
+   
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
