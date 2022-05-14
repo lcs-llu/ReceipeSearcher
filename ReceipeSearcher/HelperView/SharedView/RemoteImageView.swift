51be5c209645e3f8ad4648d5261e126a2195c9d0
Leo Lu <llu@lcs.on.ca>
2022-05-14 17:26:07 -0400

    create RemoteImageView to load image from the endpoint

---
+++ ReceipeSearcher/HelperView/SharedView/RemoteImageView.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteImageView.swift
+//  ReceipeSearcher
+//
+//  Created by Leo Lu on 2022-05-14.
+//
+
+import SwiftUI
+
+struct RemoteImageView: View {
+    let fromURL: URL
+    var body: some View {
+        AsyncImage(url: fromURL,
+                   content: { downloadedImage in
+            downloadedImage},
+                   placeholder: {
+                ProgressView()
+            })
+    }
+}
+
+struct RemoteImageView_Previews: PreviewProvider {
+    static let example = URL(string: testMeal.strMealThumb)!
+    static var previews: some View {
+        RemoteImageView(fromURL: example)
+    }
+}
